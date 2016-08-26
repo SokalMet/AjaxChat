76cfe29072fbab0b8c174f8198e55a2ce08138ed
Vi Sokal <sokalmet@ukr.net>
2016-08-26 12:21:25 +0300

    Controller is done

--- AjaxChat/Controllers/HomeController.cs
+++ AjaxChat/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AjaxChat.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,23 +9,103 @@ namespace AjaxChat.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        static ChatModel chatModel;
+
+        public ActionResult Index(string user, bool? logOn, bool? logOff, string chatMessage)
         {
-            return View();
+            try
+            {
+                if (chatModel == null)
+                {
+                    chatModel = new ChatModel();
+                }
+
+                if (chatModel.Messages.Count > 100)
+                {
+                    chatModel.Messages.RemoveRange(0, 90);
+                }
+
+                if (!Request.IsAjaxRequest())
+                {
+                    return View(chatModel);
+                }
+                else if (logOn != null && (bool)logOn)
+                {
+                    if (chatModel.Users.FirstOrDefault(u => u.Name == user) != null)
+                    {
+                        throw new Exception("Name of user is not valid");
+                    }
+                    else if (chatModel.Users.Count > 10)
+                    {
+                        throw new Exception("Chat is full");
+                    }
+                    else
+                    {
+                        chatModel.Users.Add(new ChatUser()
+                        {
+                            Name = user,
+                            LoginTime = DateTime.Now,
+                            LastPing = DateTime.Now
+                        });
+
+                        chatModel.Messages.Add(new ChatMessage()
+                        {
+                            Text = user + " entered the chat",
+                            Date = DateTime.Now
+                        });
+                    }
+                    return PartialView("ChatRoom", chatModel);
+                }
+                else if (logOff != null && (bool)logOff)
+                {
+                    LogOff(chatModel.Users.FirstOrDefault(u => u.Name == user));
+                    return PartialView("ChatRoom", chatModel);
+                }
+                else
+                {
+                    ChatUser currentUser = chatModel.Users.FirstOrDefault(u => u.Name == user);
+
+                    currentUser.LastPing = DateTime.Now;
+
+                    List<ChatUser> toRemove = new List<ChatUser>();
+                    foreach (Models.ChatUser usr in chatModel.Users)
+                    {
+                        TimeSpan span = DateTime.Now - usr.LastPing;
+                        if (span.TotalSeconds > 15)
+                            toRemove.Add(usr);
+                    }
+                    foreach (ChatUser u in toRemove)
+                    {
+                        LogOff(u);
+                    }
+
+                    if (!string.IsNullOrEmpty(chatMessage))
+                    {
+                        chatModel.Messages.Add(new ChatMessage()
+                        {
+                            User = currentUser,
+                            Text = chatMessage,
+                            Date = DateTime.Now
+                        });
+                    }
+                    return PartialView("History", chatModel);
+                }
+            }
+            catch (Exception ex)
+            {
+                Response.StatusCode = 500;
+                return Content(ex.Message);
+            }
         }
 
-        public ActionResult About()
+        public void LogOff(ChatUser user)
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
+            chatModel.Users.Remove(user);
+            chatModel.Messages.Add(new ChatMessage()
+            {
+                Text = user.Name + " leaved chat",
+                Date = DateTime.Now
+            });
+        }        
     }
 }
